4a5f7757da03b40fa09d7ba9d83e29e08a20e885
Eric Miller <eric@tallprairie.com>
2019-09-29 17:08:05 -0500

    Update to fix compatibility with certverify

--- certdump/certdump.cr
+++ certdump/certdump.cr
@@ -1,20 +1,33 @@
 # cerner_2^5_2019
-# Check site up
 host, port = ARGV[0].split(":")[0], ARGV[0].split(":")[1]? || 443
 format = ENV.fetch("FORMAT", "text")
-filename = ""
-case format
-when "pem"
-  cert = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -outform PEM`
-  filename = "#{host}.crt"
-when "chain"
-  cert = `openssl s_client -showcerts -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | sed -n '/^-----BEGIN CERT/,/^-----END CERT/p'`
-  filename = "#{host}.chain.crt"
-when "text"
-  cert = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -text -certopt ext_error,no_sigdump,no_pubkey -noout`
-  filename = "#{host}.text.crt"
-end
+cert = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -outform PEM`
 if $?.success?
-  puts cert
+  file = File.open("#{host}.cert.pem", "w")
+  file.puts(cert)
+  file.close()
+else
+  exit 1
 end
-
+chain = `openssl s_client -showcerts -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | sed -n '/^-----BEGIN CERT/,/^-----END CERT/p'`
+if $?.success?
+  file = File.open("#{host}.chain.pem", "w")
+  file.puts(chain)
+  file.close()
+else
+  exit 1
+end
+text = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -text -certopt ext_error,no_sigdump,no_pubkey -noout`
+if $?.success?
+  file = File.open("#{host}.cert.txt", "w")
+  file.puts(text)
+  file.close()
+  case format
+  when "cert"
+    puts cert
+  when "chain"
+    puts chain
+  when "text"
+    puts text
+  end
+end
