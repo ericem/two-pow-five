1ccda821dedc242ab1f7e146155e30eeccf210bb
Eric Miller <eric@tallprairie.com>
2019-09-30 20:48:44 -0500

    Update to use openssl 1.1

--- certdump/certdump.cr
+++ certdump/certdump.cr
@@ -1,7 +1,7 @@
 # cerner_2^5_2019
-host, port = ARGV[0].split(":")[0], ARGV[0].split(":")[1]? || 443
+openssl, host, port = "/usr/local/opt/openssl@1.1/bin/openssl", ARGV[0].split(":")[0], ARGV[0].split(":")[1]? || 443
 format = ENV.fetch("FORMAT", "text")
-cert = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -outform PEM`
+cert = `#{openssl} s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | #{openssl} x509 -outform PEM`
 if $?.success?
   file = File.open("#{host}.cert.pem", "w")
   file.puts(cert)
@@ -9,7 +9,7 @@ if $?.success?
 else
   exit 1
 end
-chain = `openssl s_client -showcerts -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | sed -n '/^-----BEGIN CERT/,/^-----END CERT/p'`
+chain = `#{openssl} s_client -showcerts -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | sed -n '/^-----BEGIN CERT/,/^-----END CERT/p'`
 if $?.success?
   file = File.open("#{host}.chain.pem", "w")
   file.puts(chain)
@@ -17,7 +17,7 @@ if $?.success?
 else
   exit 1
 end
-text = `openssl s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | openssl x509 -text -certopt ext_error,no_sigdump,no_pubkey -noout`
+text = `#{openssl} s_client -connect #{host}:#{port} -servername #{host} </dev/null 2>/dev/null | #{openssl} x509 -text -certopt ext_error,no_sigdump,no_pubkey -noout`
 if $?.success?
   file = File.open("#{host}.cert.txt", "w")
   file.puts(text)
